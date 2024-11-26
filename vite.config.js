e47491420f6fd56926714ed825d58d3f0cf37877
Taylor Johnson <johnsoctbus@gmail.com>
2024-11-26 10:34:59 -0500

    added build setup to vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import path from 'path'
 import { defineConfig } from "vite"
 
 export default defineConfig({
+        build: {
+                lib: {
+                        entry: 'index.js',
+                        filename: 'rolodex', // Name of the output
+                        name: 'rolodex', // Namespace of exposed global variable
+                },
+                minify: 'esbuild', // default
+                outDir: 'dist', // default
+                rollupOptions: {}, // underlying rollup config options
+                target: 'modules', // default
+        },
         resolve: {
                 alias: {
                         '@': path.resolve(__dirname, './'),
