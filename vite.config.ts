bde89d3483decb74e334be625e27ecd580bcb0e3
Chris Johnson <johnsoctbus@gmail.com>
2025-02-10 19:00:01 -0500

    set base for gh deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,38 +3,39 @@ import { resolve } from 'path'
 import { defineConfig } from "vite"
 
 export default defineConfig({
-        build: {
-                // Build only rolodex.ts into ../dist
-                emptyOutDir: true, // Clear ../dist before building
-                lib: {
-                        entry: resolve(__dirname, 'src/rolodex.ts'),
-                        name: 'rolodex',
-                },
-                minify: 'esbuild', // default for client builds
-                outDir: 'dist',
-                rollupOptions: {
-                        // Externalize unwanteed dependeancies
-                        external: [''],
-                }, // underlying rollup config options
-                target: 'modules', // default
+    base: '/rolodex',
+    build: {
+        // Build only rolodex.ts into ../dist
+        emptyOutDir: true, // Clear ../dist before building
+        lib: {
+            entry: resolve(__dirname, 'src/rolodex.ts'),
+            name: 'rolodex',
         },
-        resolve: {
-                alias: {
-                        '@': resolve(__dirname, './'),
-                        '@src': resolve(__dirname, './src'),
-                        '@test': resolve(__dirname, './tests'),
-                },
+        minify: 'esbuild', // default for client builds
+        outDir: 'dist',
+        rollupOptions: {
+            // Externalize unwanteed dependeancies
+            external: [''],
+        }, // underlying rollup config options
+        target: 'modules', // default
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './'),
+            '@src': resolve(__dirname, './src'),
+            '@test': resolve(__dirname, './tests'),
         },
-        test: {
-                browser: {
-                        enabled: true,
-                        name: 'chromium',
-                        provider: 'playwright',
-                        // testerHtmlPath: '',
-                },
-                poolMatchGlobs: [
-                        ['**/tests/web-component/**', 'browser'],
-                        ['**/tests/unit/**', 'threads'],
-                ],
+    },
+    test: {
+        browser: {
+            enabled: true,
+            name: 'chromium',
+            provider: 'playwright',
+            // testerHtmlPath: '',
         },
+        poolMatchGlobs: [
+            ['**/tests/web-component/**', 'browser'],
+            ['**/tests/unit/**', 'threads'],
+        ],
+    },
 })
